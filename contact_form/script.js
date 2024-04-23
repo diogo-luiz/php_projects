f6b8b3aee12dac529062ad74d37939f417f9e40f
Diogo Ribeiro <profissional.diogoribeiro@gmail.com>
2024-04-23 16:30:01 -0300

    Updating code

--- contact_form/script.js
+++ contact_form/script.js
@@ -1,29 +1,30 @@
+//Contact Form in PHP
 const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
 form.onsubmit = (e)=>{
-    e.preventDefault(); //preventing form from submitting
-    statusTxt.style.color = "#0D6EFD";
-    statusTxt.style.display = "block";
-    statusTxt.innerText = "Sending your message...";
-    form.classList.add("disabled");
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "message.php", true);
-    xhr.onload = ()=>{
-        if(xhr.readyState == 4 && xhr.status == 200){// if ajax response status is 200 & ready status is 4 means there is no any error
-            let response = xhr.response;
-            if(response.indexOf("Área de email e mensagem são necessários!") != -1 || response.indexOf("Insira um email válido!") != -1 || response.indexOf("Desculpe, houve um problema ao enviar seu email!") != -1){
-                statusTxt.style.color = "red";
-            }else{
-                form.reset();
-                setTimeout(()=>{
-                    statusTxt.style.display = "none";
-                }, 3000);
-            }
-            statusTxt.innerText = response;
-            form.classList.remove("disabled");
-        }
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
     }
-    let formData = new FormData(form); //creating new FormData obj. This obj is used to send form data
-    xhr.send(formData); //sending dorm data
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
 }
